2815f90b4164388bde3898114f664772053fcde8
William Marciano <bill.marciano@gmail.com>
2023-04-29 20:35:07 -0300

    Versão corrigida da função IMC

--- R/FunçãoTeste.R
+++ R/FunçãoTeste.R
@@ -8,15 +8,17 @@
   #'
   #' @export
   IMC <- function(x,y) {
+      if(x==0 || x<0)
+        return("Deixa de brincadeira e digite um peso válido")
       if(y==0 || y<0)
-      return("Deixa de brincadeira e digite uma altura válida")
+        return("Deixa de brincadeira e digite uma altura válida")
       imc=x/(y)^2
-      if(imc>18.5 || imc<24.9)
-        return("Seu IMC é:", imc, "e está NORMAL")
-      if(imc>25.0 || imc<29.9)
-        return("Seu IMC é:", imc, "e você está com SOBREPESO")
-      if(imc>30.0 || imc<39.9)
-        return("Seu IMC é:", imc, "e você está OBESO")
+      if(imc>18.5 && imc<24.9)
+        return(paste("Seu IMC é:", round(imc,2), "e está NORMAL"))
+      if(imc>25.0 && imc<29.9)
+        return(paste("Seu IMC é:", round(imc,2), "e você está com SOBREPESO"))
+      if(imc>30.0 && imc<39.9)
+        return(paste("Seu IMC é:", round(imc,2), "e você está OBESO"))
       if(imc>40.0)
-        return("Seu IMC é:", imc, "e você está com OBESIDADE GRAVE")
+        return(paste("Seu IMC é:", round(imc,2), "e você está com OBESIDADE GRAVE"))
       }
