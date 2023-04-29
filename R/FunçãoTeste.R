5b57fde0d2e66548085be699e59b7710e48b3380
William Marciano <bill.marciano@gmail.com>
2023-04-29 13:56:57 -0300

    Função soma criada.

---
+++ R/FunçãoTeste.R
@@ -0,0 +1,12 @@
+Exemplo:
+
+  #' Soma 2
+  #'
+  #' Recebe um vetor de números e retorna um vetor de números somando dois
+  #'
+  #' @param x vetor de números.
+  #'
+  #' @export
+  soma_2 <- function(x) {
+    x + 2
+  }
